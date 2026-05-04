a4f66809915d06adfb6b79ca80fe9b1d0ec4b9ad
Darsh Ameta <279248114+Dragoboy14@users.noreply.github.com>
2026-05-04 18:11:09 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -4,7 +4,7 @@ import json
 
 # --- CONFIGURATION ---
 # Apni Raw Gist URL yahan dalo (Wahi jo update.py mein thi)
-GIST_RAW_URL = "0b338953c699b52709dc1174f96e0b77"
+GIST_RAW_URL = "https://gist.github.com/Dragoboy14/0b338953c699b52709dc1174f96e0b77"
 
 st.set_page_config(page_title="Darsh AI", page_icon="🤖")
 st.title("🤖 Darsh AI (Ollama Remote)")
